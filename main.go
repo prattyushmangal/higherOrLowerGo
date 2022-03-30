4a76dbfe8f4379eb4c74b3d5973e955f7c03d180
ANUSHMANGAL01 <anushmangal01@gmail.com>
2022-03-30 18:33:17 +0530

    Added the basic GUI to the previous logic

--- main.go
+++ main.go
@@ -1,49 +1,15 @@
 package main
 
 import (
-	logic "anush22/cardgame/utils"
 	game "anush22/cardgame/src"
-	"fmt"
+	"github.com/faiface/pixel/pixelgl"
 )
 
-func main() {
-	// fmt.Println("Hello, world.")
-	// squared := logic.Something(9)
-	// fmt.Println(squared)
-	// fmt.Println(logic.Randomiser())
-	// answer:=game.GetInput()
-	// fmt.Println(answer)
-
-	var guess string
-	var prevCard string
-	var currentCard string
-	var game_score int
-	game_score=0
-	fmt.Println("Game Starts")
-	fmt.Println("Your score is " ,game_score)
-	fmt.Println("First card is : ")
-	prevCard =logic.Randomiser()
-	fmt.Println(prevCard)
-	for true{
-		currentCard = logic.Randomiser()
-		if(currentCard == "Cards Exhausted"){
-			fmt.Println("Congratulations. You Win. Your score is " , game_score)
-			break
-		}
-		fmt.Println("Time for next card")
-		fmt.Println("Enter your Guess : A or B")
-		fmt.Scanln(&guess)		
-		fmt.Println("Your next card is : ")		
-		fmt.Println(currentCard)
-		game.UpdateScore(prevCard, currentCard, guess)
-		if(game.GetScore() == game_score){
-			fmt.Println("Sorry, Wrong Guess. Game ends. Your score is " ,game_score)
-			break;
-		}
-		game_score++
-		fmt.Println("Wow! That is a Correct Guess. Your score is " ,game_score)
-		prevCard=currentCard
 
 
-	}
+func main(){
+	pixelgl.Run(game.Run)
 }
+
+
+
--- src/game.go
+++ src/game.go
@@ -3,6 +3,16 @@ package src
 import (
 	
 	logic "anush22/cardgame/utils"
+	"fmt"
+	"image"
+	"os"
+	// "math/rand"
+	// "math"
+	// "time"
+	_ "image/png"
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/pixelgl"
+	"golang.org/x/image/colornames"
 )
 
 var options = [2]string{"A", "B"}
@@ -14,6 +24,123 @@ var score = 0
 // 	fmt.Scanln(&userInput)
 // 	return userInput
 // }
+
+
+
+func loadPicture(path string) (pixel.Picture, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	img, _, err := image.Decode(file)
+	if err != nil {
+		return nil, err
+	}
+	return pixel.PictureDataFromImage(img), nil
+}
+
+func getSprite(card string) (*pixel.Sprite){
+	path:="images/card"+card+".png"
+	cardPicture, err := loadPicture(path)
+	if err != nil {
+		panic(err)
+	}
+	cardSprite := pixel.NewSprite(cardPicture, cardPicture.Bounds())
+	return cardSprite
+}
+
+func Run(){
+	cfg := pixelgl.WindowConfig{
+		Title:  "Card Guess Game!",
+		Bounds: pixel.R(0, 0, 1024, 768),
+		VSync:  true,
+	}
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	
+	var(
+		prevCard string
+		currentCard string
+		game_score int
+	)
+	game_score=0
+	prevmat := pixel.IM
+	prevmat = prevmat.Moved(pixel.V(256, 384))
+	currmat := pixel.IM
+	currmat = currmat.Moved(pixel.V(768, 384))
+	gameOverPicture, err := loadPicture("images/gameover.png")
+	gameOverSprite := pixel.NewSprite(gameOverPicture, gameOverPicture.Bounds())
+
+	prevCard = "Blank"
+	currentCard =logic.Randomiser()
+	prevSprite:=getSprite(prevCard)
+	currSprite:=getSprite(currentCard)
+
+
+	for !win.Closed() {
+
+
+		if win.JustPressed(pixelgl.KeyDown) {
+			prevCard = currentCard
+			currentCard = logic.Randomiser()
+			if(currentCard=="Cards Exhausted"){
+				gameOverSprite.Draw(win, pixel.IM.Moved(win.Bounds().Center()))
+				break;
+			}
+			prevSprite = getSprite(prevCard)
+			currSprite=getSprite(currentCard)
+			UpdateScore(prevCard, currentCard, "B")
+			if(GetScore() == game_score){
+				fmt.Println("Sorry, Wrong Guess. Game ends. Your score is " ,game_score)
+				fmt.Println(prevCard)
+				fmt.Println(currentCard)
+				gameOverSprite.Draw(win, pixel.IM.Moved(win.Bounds().Center()))
+				break;
+			}
+			game_score++
+			fmt.Println("Wow! That is a Correct Guess. Your score is " ,game_score)
+			fmt.Println(prevCard)
+			fmt.Println(currentCard)
+			
+		}
+		if win.JustPressed(pixelgl.KeyUp) {
+			prevCard = currentCard
+			currentCard = logic.Randomiser()
+			if(currentCard=="Cards Exhausted"){
+				gameOverSprite.Draw(win, pixel.IM.Moved(win.Bounds().Center()))
+				break;
+			}
+			prevSprite = getSprite(prevCard)
+			currSprite=getSprite(currentCard)
+			UpdateScore(prevCard, currentCard, "A")
+			if(GetScore() == game_score){
+				fmt.Println("Sorry, Wrong Guess. Game ends. Your score is " ,game_score)
+				fmt.Println(prevCard)
+				fmt.Println(currentCard)
+				gameOverSprite.Draw(win, pixel.IM.Moved(win.Bounds().Center()))
+				break;
+			}
+			game_score++
+			fmt.Println("Wow! That is a Correct Guess. Your score is " ,game_score)
+			fmt.Println(prevCard)
+			fmt.Println(currentCard)
+			
+		}
+
+		win.Clear(colornames.Greenyellow)
+
+		prevSprite.Draw(win, prevmat)
+		currSprite.Draw(win, currmat)
+		win.Update()
+
+
+	}
+}
+
 
 func UpdateScore(prevCard string,  currentCard string, userAnswer string){
 	if (userAnswer=="A" && logic.IsAbove(prevCard, currentCard)){
